13ec3dfa9632350cd9031a9bed1d4eca8af2f5a9
JuanPabl07DP <juanpadazer15@gmail.com>
2025-03-13 22:32:12 -0500

    Fin :(

--- src/main/java/co/edu/escuelaing/arep/securing_web/security/SecurityConfig.java
+++ src/main/java/co/edu/escuelaing/arep/securing_web/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://myapachearep.duckdns.org"));
+        configuration.setAllowedOrigins(Arrays.asList("https://mipaginajpdp.duckdns.org"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
